fefbf8c6b58519af08dcbd235aa3a2868ab412b3
Your Name <stucywebdesigns1@gmail.com>
2022-05-27 18:37:42 -0500

    added files

--- django_lesson/settings.py
+++ django_lesson/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'django_lesson.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_lesson.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
